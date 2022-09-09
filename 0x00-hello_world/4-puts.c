0266cad669ed76f3f7d7dda40a0b2e745c55c8ab
Abena Wiafe <inna.wiafe@gmail.com>
2022-09-09 06:23:14 -0700

    final changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * Description: main = Prints out a string to stdout.
+ * main - entry point
+ * Description: Prints out "Programming is like building a multilingual puzzle"
  * Return: 0 if success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
